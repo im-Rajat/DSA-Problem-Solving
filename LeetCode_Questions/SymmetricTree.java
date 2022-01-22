0b06891426d42d56a4a0ea87437750d8c86cbbe6
Rajat Kumar <20627338+im-Rajat@users.noreply.github.com>
2022-01-22 18:41:09 +0530

    101. Symmetric Tree

--- LeetCode_Questions/SymmetricTree.java
+++ LeetCode_Questions/SymmetricTree.java
@@ -19,20 +19,38 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         
-        return checkSymmetric(root, root);
+        return isMirror(root, root);
     }
     
-    public boolean checkSymmetric(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) {
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) {
             return true;
         }
         
-        if(root1 != null && root2 != null && root1.val == root2.val && checkSymmetric(root1.left, root2.right) && checkSymmetric(root1.right, root2.left)){
-            return true;
-        }
-        else {
+        if(t1 == null || t2 == null) {
             return false;
         }
-    
+        
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+        
     }
+    
+//     public boolean isSymmetric(TreeNode root) {
+        
+//         return checkSymmetric(root, root);
+//     }
+    
+//     public boolean checkSymmetric(TreeNode root1, TreeNode root2) {
+//         if(root1 == null && root2 == null) {
+//             return true;
+//         }
+        
+//         if(root1 != null && root2 != null && root1.val == root2.val && checkSymmetric(root1.left, root2.right) && checkSymmetric(root1.right, root2.left)){
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+    
+//     }
 }
