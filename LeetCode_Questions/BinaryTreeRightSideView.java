0ba8d6206be019478b1a6faa571f112279fccfb6
Rajat Kumar <20627338+im-Rajat@users.noreply.github.com>
2022-02-18 12:17:25 +0530

    199. Binary Tree Right Side View
    https://leetcode.com/problems/binary-tree-right-side-view/

--- LeetCode_Questions/BinaryTreeRightSideView.java
+++ LeetCode_Questions/BinaryTreeRightSideView.java
@@ -52,3 +52,40 @@ class Solution {
         return result;
     }
 }
+
+// // Using Reverse Level Order Traversal
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+        
+//         List<Integer> result = new ArrayList<>();
+        
+//         if (root == null) {
+//             return result;
+//         }
+        
+//         Queue<TreeNode> queue = new LinkedList<>();
+        
+//         queue.add(root);
+        
+//         while (queue.isEmpty() == false) {
+//             int size = queue.size();
+            
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode temp = queue.poll();
+//                 if (i == 0) {
+//                     result.add(temp.val);
+//                 }
+                
+//                 if (temp.right != null) {
+//                     queue.add(temp.right);
+//                 }
+                
+//                 if (temp.left != null) {
+//                     queue.add(temp.left);
+//                 }
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
