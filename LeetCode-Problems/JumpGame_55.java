9ce6f631eea6c6b043efa3529a8dc1e58f525434
Rajat Kumar <20627338+im-Rajat@users.noreply.github.com>
2022-05-18 16:43:27 +0530

    55. Jump Game
    https://leetcode.com/problems/jump-game/submissions/

---
+++ LeetCode-Problems/JumpGame_55.java
@@ -0,0 +1,20 @@
+// 55. Jump Game
+// https://leetcode.com/problems/jump-game/submissions/
+
+
+// Using Greedy Approach O(n)
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        
+        for (int i = 0; i <= reach; i++) {
+            reach = Math.max(reach, i + nums[i]);
+            
+            if (reach >= nums.length - 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
