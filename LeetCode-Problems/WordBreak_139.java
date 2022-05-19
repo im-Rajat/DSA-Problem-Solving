11e68fce5e23b0dcb2eff01d892331a0a606b79d
Rajat Kumar <20627338+im-Rajat@users.noreply.github.com>
2022-05-19 12:45:32 +0530

    139. Word Break
    https://leetcode.com/problems/word-break/

---
+++ LeetCode-Problems/WordBreak_139.java
@@ -0,0 +1,29 @@
+// 139. Word Break
+// https://leetcode.com/problems/word-break/
+
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        
+        Set<String> set = new HashSet<>();
+        set.addAll(wordDict);
+        
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (set.contains(s.substring(j, i)) && dp[j] == true) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
